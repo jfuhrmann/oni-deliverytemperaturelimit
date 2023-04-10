e6863ad68654e022f3943e2cadc2302109c5b411
Luboš Luňák <l.lunak@centrum.cz>
2023-04-10 19:27:44 +0200

    include a copy of PLib
    See README.PLib .

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,6 +1,14 @@
+using HarmonyLib;
+using PeterHan.PLib.Core;
+
 namespace DeliveryTemperatureLimit
 {
     public class Mod : KMod.UserMod2
     {
+        public override void OnLoad( Harmony harmony )
+        {
+            base.OnLoad( harmony );
+            PUtil.InitLibrary( false );
+        }
     }
 }
