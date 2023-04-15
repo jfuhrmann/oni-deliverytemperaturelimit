3247ff08421c233373bf0cd4f1ca57b44a8e3879
Luboš Luňák <l.lunak@centrum.cz>
2023-04-15 08:57:58 +0200

    use "Delivery temperatures:" as the side screen label
    Because that's what it is, it doesn't do anything once an object
    is stored.

--- Source/Strings.cs
+++ Source/Strings.cs
@@ -4,7 +4,7 @@ namespace STRINGS
 {
     public class TEMPERATURELIMITS
     {
-        public static LocString LABEL = "Temperature range:";
+        public static LocString LABEL = "Delivery temperatures:";
 
         public static LocString RANGE_SEPARATOR = "-";
     }
